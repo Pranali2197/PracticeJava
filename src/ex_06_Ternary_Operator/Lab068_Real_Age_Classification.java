e026d1cc8a4687a1bc12824514cd340c8708758a
Pranali2197 <you@example.com>
2025-05-19 21:06:25 +0530

    ex_07_Increment_Decrement_Op

--- src/ex_06_Ternary_Operator/Lab068_Real_Age_Classification.java
+++ src/ex_06_Ternary_Operator/Lab068_Real_Age_Classification.java
@@ -1,12 +1,17 @@
-package ex_06_Ternary_Operator;
+package  ex_06_Ternary_Operator ;
+
 
 public class Lab068_Real_Age_Classification {
     public static void main(String[] args) {
 
         // age = 23 -> Adult  ( age > 18) , Minor (age  <18), Senior ( age > 65)
 
-            String user_input = args[0];
-            System.out.println(user_input instanceof String);
+
+             String user_input = args[0];
+
+
+             //String user_input = args[1];
+           System.out.println(user_input instanceof String);
             System.out.println(user_input);
 
             int age = Integer.parseInt(user_input); // NumberFormatException: For input string: "pramod"
