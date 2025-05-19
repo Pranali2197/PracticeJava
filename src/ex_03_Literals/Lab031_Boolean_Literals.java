f24d783e460c8f04dc7a1c148c8b5fc47a4a0604
Pranali2197 <you@example.com>
2025-05-19 15:41:16 +0530

    ex_03_Literals

---
+++ src/ex_03_Literals/Lab031_Boolean_Literals.java
@@ -0,0 +1,11 @@
+package ex_03_Literals;
+
+public class Lab031_Boolean_Literals {
+    public static void main(String[] args) {
+
+        boolean is_havecar = true;
+        boolean is_havehome=false;
+
+        //boolean is_havegoodpercnt = 123;//java: incompatible types: int cannot be converted to boolean
+    }
+}
