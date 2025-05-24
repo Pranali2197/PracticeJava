1b512b319479f08f0d5846bb58556bd76d8adbf8
Pranali2197 <you@example.com>
2025-05-25 00:25:16 +0530

    ex_10_For_Loop

---
+++ src/ex_10_For_Loop/Lab107_ForLoop_2.java
@@ -0,0 +1,17 @@
+package ex_10_For_Loop;
+
+public class Lab107_ForLoop_2 {
+    public static void main(String[] args) {
+        for ( long i=11; i<10;i++){
+
+            System.out.println(i);
+        }
+
+        for(float f = 10 ; f>10;f++){
+            System.out.println("Hi float->" +f);
+
+        }
+    }
+
+
+}
